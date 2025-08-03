d326c59d3ddb945dc99cc64e542f4e7ecd63b1a6
Thanes <117477122+thanesar@users.noreply.github.com>
2025-08-03 13:01:27 +0000

    Made new commit

--- LilL_Monsters_Site/js/script.js
+++ LilL_Monsters_Site/js/script.js
@@ -6,9 +6,7 @@ $(function () {
   let monsterScroll = document.querySelectorAll('#monster-group .monster')
   let navHeight = nav.scrollHeight
 
-  monsterScroll.forEach(
-    (item) => (item.style.animationDelay = `${Math.random() * 1 + 0.4}s`)
-  )
+  
 
   function inViewPort(el) {
     let rect = el.getBoundingClientRect()
